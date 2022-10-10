a148dc52fe533ce92e86e833889b498b53eca6b0
AlexisLuna28 <choche_.28@hotmail.com>
2022-10-10 17:51:45 -0500

    Update clase 1

--- Clases/Clase 1.R
+++ Clases/Clase 1.R
@@ -1,11 +1,12 @@
-> # Jorge Alexis Luna Robles
-  > # 09/08/2022
-  > # Clase 1
-  > # sirve para realizar comentarios
-  > # operaciones basicas
-  > 2 * 2 
+# Jorge Alexis Luna Robles
+# 09/08/2022
+# Clase 1
+# sirve para realizar comentarios
+# operaciones basicas
+2 * 2 
 
 h <- 2
+
 h * h
 
 g <-c(3, 5, 8, 9)
